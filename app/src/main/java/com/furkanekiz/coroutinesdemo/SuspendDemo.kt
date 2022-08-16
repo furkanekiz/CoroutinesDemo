e583dad5cf16c88d97a6f2ab87cbd119468d7442
furkanekiz <furkanekiz.06@gmail.com>
2022-08-16 10:27:33 +0300

    suspending functions

---
+++ app/src/main/java/com/furkanekiz/coroutinesdemo/SuspendDemo.kt
@@ -0,0 +1,12 @@
+package com.furkanekiz.coroutinesdemo
+
+class SuspendDemo {
+
+    private fun firstFunction(){
+        //some code
+    }
+
+    private suspend fun secondFunction(){
+        //some code
+    }
+}
